c7ebfae7521abb4c6f0eb5ed70ff654125185fd8
9尾雪狐 <gameclamp@gmail.com>
2019-04-13 02:13:47 +0800

    Create removeBJH.meta.js

---
+++ removeBJH.meta.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name         removeBJH
+// @namespace    https://github.com/gameclamp/removeBJH
+// @homepageURL  https://github.com/gameclamp/removeBJH
+// @supportURL   https://github.com/gameclamp/removeBJH/issues
+// @version      0.1
+// @description  
+// @author       9尾雪狐(gameclamp)
+// @match        https://www.baidu.com/*
+// @downloadURL  https://github.com/gameclamp/removeBJH/raw/master/removeBJH.user.js
+// @updateURL    https://github.com/gameclamp/removeBJH/raw/master/removeBJH.meta.js
+// @grant        none
+// @run-at       document-start
+// ==/UserScript==
